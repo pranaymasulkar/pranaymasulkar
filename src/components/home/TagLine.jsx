5c855cc6e686ff41c3cd18bd3be6da36f4261834
pranay <vickymasulkar@gmail.com>
2025-10-04 22:05:56 +0530

    update the layout settings

--- src/components/home/TagLine.jsx
+++ src/components/home/TagLine.jsx
@@ -7,11 +7,11 @@ const TagLine = () => {
             <div className='w-full md:w-[80%] text-center mx-auto'>
                 <h3 className='mb-10 text-2xl md:text-5xl leading-[1.7] tracking-widest text-gray-300'>The world of web design and development is constantly evolving, and I keep pace with the latest trends and technologies. My focus as a frontend developer is to craft digital experiences that combine stunning design with smooth, responsive functionality.</h3>
 
-                <div className='flex gap-3 justify-center'>
-                    <Link to="/works" className="rounded-full tracking-widest capitalize font-bold px-6 py-3 text-xl/6 text-[#161616] hover:bg-white ring-1 ring-gray-300 bg-gray-300 hover:ring-white me-3">
+                <div className='flex flex-col md:flex-row gap-8 md:gap-3 justify-center'>
+                    <Link to="/works" className="block md:inline-block rounded-full tracking-widest capitalize font-bold px-6 py-3 text-xl md:text-xl/6 text-[#161616] hover:bg-white ring-1 ring-gray-300 bg-gray-300 hover:ring-white md:me-3">
                         My Works
                     </Link>
-                    <a href="/CV_Pranay_Masulkar.pdf" target="_blank" rel="noopener noreferrer" className="rounded-full tracking-widest px-6 py-3 text-xl/6 text-gray-300 hover:text-white ring-2 ring-gray-300 hover:ring-white">
+                    <a href="/CV_Pranay_Masulkar.pdf" target="_blank" rel="noopener noreferrer" className="block md:inline-block rounded-full tracking-widest px-6 py-3 text-xl md:text-xl/6 text-gray-300 hover:text-white ring-2 ring-gray-300 hover:ring-white">
                         Download CV
                     </a>
                 </div>
